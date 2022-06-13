ffd15307ee39dda0c24f53a6f4ac6c830f5678f7
Natsuko <makumaku2014@gmail.com>
2022-06-13 18:47:24 +0900

    修正後のコミット1回目

--- ThirdTimeTasek/src/Main.java
+++ ThirdTimeTasek/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         List<String> needToDo = new ArrayList<String>();
 
